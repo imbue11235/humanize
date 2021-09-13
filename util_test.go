8ec4299e69ec736a28367b3432f98d47d5acc1da
Kaspar Birk <kaspar.birk@gmail.com>
2021-09-13 19:17:48 +0200

    trunc float test

---
+++ util_test.go
@@ -0,0 +1,22 @@
+package humanize
+
+import "testing"
+
+func TestTruncFloat(t *testing.T) {
+	tests := []struct {
+		input, expected float64
+		precision       int
+	}{
+		{1.242424, 1.2, 1},
+		{5.320495, 5.32, 2},
+		{8.324343, 8.324, 3},
+		{10.2424266, 10.2424, 4},
+		{555.32323335235, 555.32323, 5},
+	}
+
+	for _, test := range tests {
+		if trunc := truncFloat(test.input, test.precision); trunc != test.expected {
+			t.Errorf("expected `%f` but got `%f`", test.expected, trunc)
+		}
+	}
+}
