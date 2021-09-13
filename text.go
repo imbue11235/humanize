0eef941bfad400b0d218648a3243ac67e6dec70d
Kaspar Birk <kaspar.birk@gmail.com>
2021-09-13 15:55:45 +0200

    fuzzy text rename

---
+++ text.go
@@ -0,0 +1,66 @@
+package humanize
+
+import (
+	"strings"
+
+	"github.com/imbue11235/words"
+)
+
+// FuzzyText formats a fuzzy text as a common sentence,
+// capitalizing the first letter of the first word, and
+// lower-casing the rest
+//
+//		s := humanize.Sentence("some_random-sentence")
+//		fmt.Print(s) => "Some random sentence"
+//
+func FuzzyText(input string) string {
+	return FormatFuzzyText(input, func(index int, word string) string {
+		if index == 0 {
+			return strings.Title(word)
+		}
+
+		return strings.ToLower(word)
+	})
+}
+
+// FormatFuzzyText extracts words from a fuzzy text and constructs
+// a string from the words, using the provided formatter on every
+// extracted word.
+//
+//		s:= humanize.FormatWords("some_random-sentence", strings.ToUpper)
+//		fmt.Print(s) => "SOME RANDOM SENTENCE"
+//
+func FormatFuzzyText(input string, formatter interface{}) string {
+	extractedWords := words.Extract(input)
+
+	var sb strings.Builder
+	for i, word := range extractedWords {
+		if i != 0 {
+			sb.WriteString(" ")
+		}
+
+		if formatter == nil {
+			sb.WriteString(word)
+			continue
+		}
+
+		sb.WriteString(useFormatter(formatter, word, i))
+	}
+
+	return sb.String()
+}
+
+// useFormatter tries to cast the formatter to one of the two function definitions accepted
+// and calls it with given parameters. if the casting is unsuccessful, the parameter string value
+// is returned
+func useFormatter(formatter interface{}, value string, index int) string {
+	if formatFunc, ok := formatter.(func(int, string) string); ok {
+		return formatFunc(index, value)
+	}
+
+	if formatFunc, ok := formatter.(func(string) string); ok {
+		return formatFunc(value)
+	}
+
+	return value
+}
---
+++ text_test.go
@@ -0,0 +1,54 @@
+package humanize
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestFormatFuzzyText(t *testing.T) {
+	tests := []struct {
+		input, expected string
+		formatter       interface{}
+	}{
+		{
+			"formatting_test",
+			"FORMATTING TEST",
+			strings.ToUpper,
+		},
+		{
+			"@@@@@a.b.c!!!!1.2.3",
+			"a b c 1 2 3",
+			nil,
+		},
+		{
+			"some-----text-----here",
+			"Some Text Here",
+			strings.Title,
+		},
+	}
+
+	for _, test := range tests {
+		if actual := FormatFuzzyText(test.input, test.formatter); actual != test.expected {
+			t.Errorf("expected `%s` but got `%s`", test.expected, actual)
+		}
+	}
+}
+
+func TestFuzzyText(t *testing.T) {
+	tests := []struct {
+		input, expected string
+	}{
+		{"some_random-Sentence", "Some random sentence"},
+		{"App_Id", "App id"},
+		{"Field-name", "Field name"},
+		{"PascalCase", "Pascal case"},
+		{"some-!!@@----Wierd_____format", "Some wierd format"},
+	}
+
+	for _, test := range tests {
+		if actual := FuzzyText(test.input); actual != test.expected {
+			t.Errorf("expected `%s` but got `%s`", test.expected, actual)
+		}
+	}
+
+}
